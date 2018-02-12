413c3ee735faf0dba7a36efac0d75944b1dc609a
Artur <art.yumaev@gmail.com>
2018-02-13 02:49:25 +0300

    Update

--- lab1/main.py
+++ lab1/main.py
@@ -3,6 +3,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+
 class Approximator(object):
 
     def __init__(self):
@@ -20,8 +21,8 @@ class Approximator(object):
 
         # Representation parameters
         plt.title("Approximation of Dirac function in segment [{0}, {1}, {2}]".format(min(x),
-                                                                                 max(x),
-                                                                                 self.segment_n_points))
+                                                                                      max(x),
+                                                                                      self.segment_n_points))
         plt.xlabel("x")
         plt.ylabel("y")
         plt.grid(True)
@@ -31,7 +32,7 @@ class Approximator(object):
         plt.plot(x_approx, y_approx, "r")
         plt.plot(x0, y0, "og")
         
-        # Plotting an annotation approximated value
+        # Plotting an annotation of approximated value
         plt.annotate('Predicted value: {:4.5f}'.format(y0),
                      xy=(x0, y0),
                      xytext=(max(x * 0.6), max(y) * 0.3),
@@ -39,43 +40,61 @@ class Approximator(object):
                                      connectionstyle="arc3,rad=-0.2",
                                      fc="w"))
 
+        # Plotting a legend
         plt.legend(["Dirac function", "Approximated function", "Predicted value"])
-
-
-    def get_parameter_alpha(self, x, y):
-        if len(x) == 2:
-            return (y[0] - y[1]) / (x[0] - x[1])
-        else:
-            return (f(x[:-1], y[:-1]) - f(x[1:], y[1:])) / (x[0] - x[-1])
 
 
     def approximate(self, x, y, x0, degree):
 
+        # Approximation module
+
         def dot_product(x0, x):
+
+            # For given x0 and vector x returs value like:
+            # x  - [x_1, x_2, ..., x_N]
+            # x0 - scalar
+            # return (x0 - x_1) * (x0 - x_2) * ... * (x0 - x_N)
+            
             product = 1
+
             for x_i in x:
                 product *= (x0 - x_i)
+
             return product
 
         def polynof_coef(x, y):
-            if len(x) == 1 or len(x) == 0:
+            
+            # Alpha parameters:
+            #
+            # For 2: y(x_i, x_j) = (y_i - y_j) / (x_i - x_j)
+            # For 3: y(x_i, x_j, x_k) = (y(x_i, x_j) - y(x_j, x_k)) / (x_i - x_k)
+            # ...
+            # For N: y(x_1, ..., x_N) = ((x_1, ..., x_N-1) - y(x_2, ..., x_N)) / (x_1 - x_N)
+
+            if ((len(x) == 1) or (len(x) == 0)):
                 return 1
-            if len(x) == 2:
+
+            if (len(x) == 2):
                 return (y[0] - y[1]) / (x[0] - x[1])
             else:
                 return (polynof_coef(x[:-1], y[:-1]) - polynof_coef(x[1:], y[1:])) / (x[0] - x[-1])
 
         def calculate_approximation(x, y, x0, n):
+
+            # Returns an approximation like:
+            #
+            # Pn(x) = y0 + (x-x0)y(x0,x1) + (x-x0)(x-x1)y(x0,x1,x2) + (x-x0)(x-x1)(x-x2)y(x0,x1,x2,x3) ...
+            
             sigma = y[0]
             for k in range(1, n + 1):
                 sigma += dot_product(x0, x[:k]) * polynof_coef(x[:k + 1], y[:k + 1])
+
             return sigma
 
 
         fitted_value = calculate_approximation(x, y, x0, degree)
 
         return fitted_value
-
 
 
     def interpolate(self):
@@ -140,21 +159,11 @@ class Approximator(object):
         return N_nearest
 
 
-    def dot(self, x, x_vector):
-        product = 1
-
-        for x_i in x_vector:
-            product *= (x - x_i)
-
-        return product
-
-
     def run(self):
 
         # Getting original Dirac function
         self.segment_n_points = 100
         self.N_nearest = self.segment_n_points // 5
-
 
         X = self.get_x_segment(n_points=self.segment_n_points)
         y = self.get_y_segment(X)
@@ -163,24 +172,29 @@ class Approximator(object):
         degree, x0 = self.read_input()
 
         # Getting N nearest of x0 points
-        N_nearest_list = self.get_x0_nearest(x0, X, self.N_nearest)
-        p = np.array(N_nearest_list)
-        q = self.get_y_segment(p)
-        o = []
-        for i in N_nearest_list:
-            o.append(self.approximate(p, q, i, degree))
+        N_nearest_list = np.array(self.get_x0_nearest(x0, X, self.N_nearest))
+        y_on_N_nearest = self.get_y_segment(N_nearest_list)
 
-        print(N_nearest_list)
+        result = []
+        for n in N_nearest_list:
+            result.append(self.approximate(N_nearest_list, y_on_N_nearest, n, degree))
+
         # Approximation on current segment
-        self.plot(X, y, p, o, x0, self.approximate(p, q, x0, degree))
+        self.plot(X,
+                  y,
+                  N_nearest_list,
+                  result,
+                  x0,
+                  self.approximate(N_nearest_list,
+                                   y_on_N_nearest,
+                                   x0,
+                                   degree))
         plt.show()
 
 
-i = Approximator()
-i.run()
+def main():
+    i = Approximator()
+    i.run()
 
-
-
-
-
-    
+if __name__ == '__main__':
+    main()
