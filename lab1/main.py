5fcaa31454520b0c8b1ecf4147731d2d1e2520ff
Artur <art.yumaev@gmail.com>
2018-02-13 00:30:31 +0300

    Update

--- lab1/main.py
+++ lab1/main.py
@@ -1,3 +1,5 @@
+# Artur Yumaev IU7-45 Computation Algorithms lab1 Dirac function approximation
+
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -10,20 +12,26 @@ class Approximator(object):
     def read_input(self):
         self.degree = int(input("Input Pn(X) degree: "))
         self.x0 = float(input("Input x0: "))
+
+        return self.degree, self.x0
     
 
-    def plot(self, x, y, y_approximated):
+    def plot(self, x, y, x_approx, y_approx, x0, y0):
 
         # Representation parameters
-        plt.title("Approximation of Dirac function in segment [{0}, {1}]".format(min(x), max(x)))
+        plt.title("Approximation of Dirac function in segment [{0}, {1}, {2}]".format(min(x),
+                                                                                 max(x),
+                                                                                 self.segment_n_points))
         plt.xlabel("x")
         plt.ylabel("y")
         plt.grid(True)
 
         # Plotting original function
-        plt.plot(x, y, "ob")
+        plt.plot(x, y, "o-", color="#A9A9A9", lw=1)
+        plt.plot(x_approx, y_approx, "r")
+        plt.plot(x0, y0, "og")
 
-        plt.legend(["Dirac function"])
+        plt.legend(["Dirac function", "Approximated function"])
         #plt.plot(x, y_approximated, "r")
 
 
@@ -32,8 +40,6 @@ class Approximator(object):
             return (y[0] - y[1]) / (x[0] - x[1])
         else:
             return (f(x[:-1], y[:-1]) - f(x[1:], y[1:])) / (x[0] - x[-1])
-
-
 
 
     def approximate(self, x, y, x0, degree):
@@ -73,7 +79,7 @@ class Approximator(object):
         pass
 
 
-    def get_x_segment(self, a=-15, b=15, n_points=200):
+    def get_x_segment(self, n_points, a=-15, b=15):
 
         # Getting linear space of x values from a to b, splitted by n_points
         return np.linspace(a, b, n_points)
@@ -88,6 +94,45 @@ class Approximator(object):
         # Dirac function
         return np.sin(x) / x
 
+
+    def get_x0_nearest(self, x0, x, N):
+
+        if N % 2 != 0:
+            N += 1
+
+        # Searching for N neartest of x0
+        x_prev = 0
+        x_next = 0
+        N_nearest = []
+        
+        for i in range(0, len(x) - 1):
+            if x0 > x[i] and x0 < x[i + 1]:
+                x_prev = x[i]
+                x_next = x[i + 1]
+
+                tmp_c = 0
+                t = 0
+                j = -1
+                while t != N // 2 and j != 0:
+                    j = i + tmp_c
+                    N_nearest.append(x[j])
+                    tmp_c -= 1
+                    t += 1
+                N_nearest = [k for k in reversed(N_nearest)]
+                
+                tmp_c = 0
+                t = 0
+                j = -1
+                while t != N // 2 and j != 0:
+                    j = i + tmp_c
+                    N_nearest.append(x[j + 1])
+                    tmp_c += 1
+                    t += 1
+                break # 2 nearest values was found
+
+        return N_nearest
+
+
     def dot(self, x, x_vector):
         product = 1
 
@@ -98,15 +143,33 @@ class Approximator(object):
 
 
     def run(self):
-        self.read_input()
-        x = self.get_x_segment()
-        y = self.get_y_segment(x)
-        self.plot(x, y, 1)
+
+        # Getting original Dirac function
+        self.segment_n_points = 100
+        self.N_nearest = self.segment_n_points // 5
+
+
+        X = self.get_x_segment(n_points=self.segment_n_points)
+        y = self.get_y_segment(X)
+        
+        # Input parameters
+        degree, x0 = self.read_input()
+
+        # Getting N nearest of x0 points
+        N_nearest_list = self.get_x0_nearest(x0, X, self.N_nearest)
+        p = np.array(N_nearest_list)
+        q = self.get_y_segment(p)
+        o = []
+        for i in N_nearest_list:
+            o.append(self.approximate(p, q, i, degree))
+
+        print(N_nearest_list)
+        # Approximation on current segment
+        self.plot(X, y, p, o, x0, self.approximate(p, q, x0, degree))
         plt.show()
 
 
 i = Approximator()
-print(i.approximate([0, 1, 2, 3], [0, 0.5, 0.866, 1], 1.5, 3))
 i.run()
 
 
