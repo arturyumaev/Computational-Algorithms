0020e2efc19419f9424801281c03bbc79bcc66da
artur <soerty>
2018-03-29 22:04:31 +0300

    update

--- lab4/main.py
+++ lab4/main.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+
 def f(x):
     return np.sin(x)
+
 
 def get_table(a, b, ndots):
     table = np.zeros((3, ndots)) # 3 is x, y, w
@@ -25,11 +27,47 @@ def get_table(a, b, ndots):
 
     return table
 
-def plot_approximated(x, y, legend):
+
+def phi(x, k, y=False, with_y=False):
+    # a - numpy vector of coefficients
+    # x - numpy vector
+    # k - degree
+    # Ф(x, a) = sum(a_k * ф_k(x))_i for i=(0, ..., n)
+    if (with_y == False):
+        return np.sum(x ** k)
+    else:
+        return np.sum((x ** k) * y)
+
+
+def get_slae(x, y, k):
+    # Create matrix A and b
+    A = np.zeros((k + 1, k + 1))
+    b = np.zeros((k + 1, 1))
+
+    # A formation
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            A[i][j] = phi(x, i + j)
+
+    # b formation
+    for i in range(b.shape[0]):
+        b[i] = phi(x, i, y=y, with_y=True)
+
+    return A, b
+
+
+def solve_slae(A, b):
+    # Returns a0, ..., ak coeficients
+    return np.linalg.solve(A, b)
+
+
+def plot_approximated(x, y, x_fitted, y_fitted, legend=['y=f(x)', 'Approximated Ф(x)~f(x)']):
+    plt.title('Function approximation')
     plt.grid(True)
     plt.legend(legend)
-    plt.plot(x, y, "r")
+    plt.plot(x, y, x_fitted, y_fitted, "r")
     plt.show()
+
 
 def change_weight(table):
     w_idx = int(input("Input weight index in the table: "))
@@ -41,8 +79,10 @@ def change_weight(table):
     
     return table
 
+
 def print_table(table):
     print(pd.DataFrame(table.T, columns=['x', 'y=sin(x)', 'w']))
+
 
 def set_params():
     a = float(input("Input a: ") or -10)
@@ -52,21 +92,37 @@ def set_params():
 
     return a, b, ndots, degree
 
-def fact(n):
-    if (n == 0):
-        return 1
-    return n * fact(n - 1)
 
-def diffs(x, x_vector):
-    m = len(x_vector)
+def compute_polynom(x, coefs):
+    fitted_value = coefs[0]
+    for i in range(1, len(coefs)):
+        fitted_value = coefs[i] * (x ** i)
 
-    return (1 / fact(m - 1)) * (f(x) ** (m - 1))
+    return fitted_value
 
-def main():
-    a, b, ndots, degree = set_params()
-    table = get_table(a, b, ndots)
-    print_table(table)
-    table = change_weight(table)
 
+def get_fitted_space(x, coefs, ndots):
+    y = np.array([compute_polynom(i, coefs) for i in x])
+
+    return x, y
+
+
+# Set main configs
+a, b, ndots, degree = set_params()
+table = get_table(a, b, ndots)
+print_table(table)
+table = change_weight(table)
+
+# System solving
+x = table[0]
+y = table[1]
+A, B = get_slae(x, y, degree)
+coefficients = solve_slae(A, B)
+x_fitted, y_fitted = get_fitted_space(x, coefficients, ndots)
+
+# Plotting results
+plot_approximated(x, y, x_fitted, y_fitted)
+"""
 if __name__ == "__main__":
     main()
+"""
